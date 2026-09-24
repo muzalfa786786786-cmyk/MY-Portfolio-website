ab227e722c690b71efe0f612859c36babd91a52a
Muzalfa BiBi <234332731+muzalfa786786786-cmyk@users.noreply.github.com>
2026-09-24 09:20:59 +0500

    Update Home.js

--- src/Home.js
+++ src/Home.js
@@ -1,3 +1,5 @@
+/* eslint-disable no-script-url */
+
 import React, { useState, useCallback } from "react";
 import { useNavigate } from "react-router-dom";
 import profileImage from "./assets/profile.png";
